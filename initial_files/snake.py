69bd744739a4094a115c0d4e2f7a1a94baa3eb21
liushui1986 <liushui1986@SHUAIPENG.localdomain>
2024-08-28 21:18:43 -0500

    initial files added, part1

---
+++ initial_files/snake.py
@@ -0,0 +1,48 @@
+import turtle
+from turtle import Turtle
+
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake():
+
+    def __init__(self):
+        self.snake = []
+        self.create_snake()
+        self.head = self.snake[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            snake_ = Turtle(shape='square')
+            snake_.color('white')
+            snake_.penup()
+            snake_.goto(position)
+            self.snake.append(snake_)
+
+    def move(self):
+        for i in range(len(self.snake) - 1, 0, -1):
+            new_x = self.snake[i - 1].xcor()
+            new_y = self.snake[i - 1].ycor()
+            self.snake[i].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.seth(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.seth(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.seth(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.seth(RIGHT)
