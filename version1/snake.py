283a430a0be889fd7b6574e4c26f5d5b42efb96f
liushui1986 <liushui1986@SHUAIPENG.localdomain>
2024-08-29 21:17:54 -0500

    version1 snake can extend and collision causes game over

---
+++ version1/snake.py
@@ -0,0 +1,54 @@
+import turtle
+from turtle import Turtle
+
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake():
+
+    def __init__(self):
+        self.snakes = []
+        self.create_snake()
+        self.head = self.snakes[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            self.add_snake(position)
+
+    def add_snake(self, position):
+        snake_ = Turtle(shape='square')
+        snake_.color('white')
+        snake_.penup()
+        snake_.goto(position)
+        self.snakes.append(snake_)
+
+    def grow(self):
+        self.add_snake(self.snakes[-1].position())
+
+    def move(self):
+        for i in range(len(self.snakes) - 1, 0, -1):
+            new_x = self.snakes[i - 1].xcor()
+            new_y = self.snakes[i - 1].ycor()
+            self.snakes[i].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.seth(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.seth(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.seth(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.seth(RIGHT)
